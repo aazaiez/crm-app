d80ce38e6f3a0535a74477d78683b73491c85a8d
Akram <azaiezakrem@gmail.com>
2018-07-26 22:04:03 -0700

    added button to display all users on hoepage

--- crm-app/src/app/app-routing.module.ts
+++ crm-app/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
 import { UsersComponent } from './users/users.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 
 const routes: Routes = [
-  { path: '', component: NavbarComponent},
   { path: 'users', component: UsersComponent},
   { path: 'users/:id', component: UserDetailsComponent}
 ];
